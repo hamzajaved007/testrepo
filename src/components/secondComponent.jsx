41c1b9020d0f54a30368b9d252f150a16765be31
Hamza <itshamzajaved@gmail.com>
2024-10-06 07:04:07 +0500

    changes to component 2

--- src/components/secondComponent.jsx
+++ src/components/secondComponent.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const SecondComponent = () => {
   return (
-    <div>SecondComponent</div>
-  )
-}
+    <div>
+      <h2>Homepage component</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam
+        similique unde accusantium et totam porro nihil, obcaecati hic iusto
+        dolorum? Laborum unde veniam in asperiores dolore ullam totam natus
+        ipsum.
+      </p>
+    </div>
+  );
+};
 
-export default SecondComponent
+export default SecondComponent;
